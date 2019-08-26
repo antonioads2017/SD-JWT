229e67ec720c26d214ad470bc957b46212dd6ca9
antonio <antoniofilhonerde@gmail.com>
2019-08-26 16:31:56 -0300

    criando projeto

---
+++ SD-jwt/src/main/java/br/edu/ifpb/SDjwt/SdJwtApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.SDjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdJwtApplication.class, args);
+	}
+
+}
